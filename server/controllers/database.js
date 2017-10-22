a95b69d35c910fc754c259a9967e874dab939789
Aaron Barber <asbarber@umich.edu>
2017-10-22 01:19:12 -0700

    [Server] Politician and organization images

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -56,17 +56,20 @@ var electionsDb = new Database([
 			{
 				"personId" : 0,
 				"name" : "Hillary Clinton",
-				"party" : "Democratic"
+				"party" : "Democratic",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg/220px-Hillary_Clinton_official_Secretary_of_State_portrait_crop.jpg"
 			},
 			{
 				"personId" : 1,
 				"name" : "Donald Trump",
-				"party" : "Republican"
+				"party" : "Republican",
+				"imageUrl" : "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/05/12/104466932-PE_Color.240x240.jpg"
 			},
 			{
 				"personId" : 2,
 				"name" : "Jill Stein",
-				"party" : "Green"
+				"party" : "Green",
+				"imageUrl" : "https://twt-media.washtimes.com/media/image/2016/08/17/jill_stein_b.jpg"
 			}
 		],
 		"title" : "2016 Presidential Election",
@@ -82,17 +85,20 @@ var electionsDb = new Database([
 			{
 				"personId" : 3,
 				"name" : "Catherine Cortez Masto",
-				"party" : "Democratic"
+				"party" : "Democratic",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Catherine_Cortez_Masto_official_portrait.jpg/1200px-Catherine_Cortez_Masto_official_portrait.jpg"
 			},
 			{
 				"personId" : 4,
 				"name" : "Joe Heck",
-				"party" : "Republican"
+				"party" : "Republican",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/141212-A-ZP772-001.jpeg/220px-141212-A-ZP772-001.jpeg"
 			},
 			{
 				"personId" : 5,
 				"name" : "Tom Jones",
-				"party" : "Independent"
+				"party" : "Independent",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/c/c8/Chris_Collins_official_photo.jpg"
 			}
 		],
 		"title" : "2016 Nevada U.S. Senate",
@@ -108,17 +114,20 @@ var electionsDb = new Database([
 			{
 				"personId" : 6,
 				"name" : "Dina Titus",
-				"party" : "Democratic"
+				"party" : "Democratic",
+				"imageUrl" : "https://pbs.twimg.com/profile_images/669584452322877442/z7pOtlJI.jpg"
 			},
 			{
 				"personId" : 7,
 				"name" : "Mark Amodei",
-				"party" : "Republican"
+				"party" : "Republican",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Mark_Amodei.jpg/220px-Mark_Amodei.jpg"
 			},
 			{
 				"personId" : 8,
 				"name" : "Cresent Hardy",
-				"party" : "Independent"
+				"party" : "Independent",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/2015-01-08_OfficialPhoto_RepCresentHardy_NV04.jpg/220px-2015-01-08_OfficialPhoto_RepCresentHardy_NV04.jpg"
 			}
 		],
 		"title" : "2016 Nevada U.S. House",
@@ -134,17 +143,20 @@ var electionsDb = new Database([
 			{
 				"personId" : 9,
 				"name" : "Oscar Goodman",
-				"party" : "Democratic"
+				"party" : "Democratic",
+				"imageUrl" : "http://www.vegas24seven.com/wp-content/uploads/2011/11/O.Goodman-LVCVA-Headshot.jpg"				
 			},
 			{
 				"personId" : 10,
 				"name" : "Arnie Adamsen",
-				"party" : "Republican"
+				"party" : "Republican",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/8/8f/Scott_Taylor_official_photo.jpg"
 			},
 			{
 				"personId" : 11,
 				"name" : "Kate Hallberg",
-				"party" : "Independent"
+				"party" : "Independent",
+				"imageUrl" : "https://i.pinimg.com/236x/37/6d/fa/376dfa0c31bd285e2bb0d139427a860b--politicians-the-list.jpg"
 			}
 		],
 		"title" : "2016 City of Las Vegas Judge",
@@ -160,17 +172,20 @@ var electionsDb = new Database([
 			{
 				"personId" : 12,
 				"name" : "Carolyn Goodman",
-				"party" : "Democratic"
+				"party" : "Democratic",
+				"imageUrl" : "https://www.myvegasmag.com/os/resources/media/mayor_sp14.jpg"
 			},
 			{
 				"personId" : 13,
 				"name" : "Starvos Anthony",
-				"party" : "Republican"
+				"party" : "Republican",
+				"imageUrl" : "http://www.watchdogwag.com/wp-content/uploads/2015/01/Stavros-III.jpg"
 			},
 			{
 				"personId" : 14,
 				"name" : "Phil Cory",
-				"party" : "Independent"
+				"party" : "Independent",
+				"imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Jim_Davis.jpg/1200px-Jim_Davis.jpg"
 			}
 		],
 		"title" : "2016 City of Las Vegas Mayor",
@@ -187,49 +202,56 @@ var organizationsDb = new Database([
 		"title" : "Clinton Foundation",
 		"description" : "We believe that the best way to unlock human potential is through the power of creative collaboration. That's why we build partnerships between businesses, NGOs, governments, and individuals everywhere to work faster, leaner, and better; to find solutions that last; and to transform lives and communities from what they are today to what they can be, tomorrow.",
 		"relatedPersonIds" : [0, 3, 6, 9, 12],
-		"totalDonations" : "$7.1m"		
+		"totalDonations" : "$7.1m",
+		"imageUrl" : "http://www.clintonfoundation.org/sites/all/themes/custom/cf/cf-logo-big.png"	
 	},
 	{
 		"organizationId" : 2,
 		"title" : "American Civil Liberties Union",
 		"description" : "The American Civil Liberties Union was founded in 1920 and is our nation's guardian of liberty. The ACLU works in the courts, legislatures and communities to defend and preserve the individual rights and liberties guaranteed to all people in this country by the Constitution and laws of the United States.",
 		"relatedPersonIds" : [0, 3, 6, 9, 12],
-		"totalDonations" : "$5.7m"		
+		"totalDonations" : "$5.7m",
+		"imageUrl" : "https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/American_Civil_Liberties_Union_logo.svg/1280px-American_Civil_Liberties_Union_logo.svg.png"
 	},
 	{
 		"organizationId" : 3,
 		"title" : "National Organization for Women",
 		"description" : "The National Organization for Women is the largest organization of feminist grassroots activists in the United States. NOW has hundreds of chapters and hundreds of thousands of members and activists in all 50 states and the District of Columbia.",
 		"relatedPersonIds" : [0, 3, 6, 9, 12],
-		"totalDonations" : "$9.8m"		
+		"totalDonations" : "$9.8m",
+		"imageUrl" : "http://bostonnow.org/wp-content/uploads/2016/09/NOW.png"	
 	},
 	{
 		"organizationId" : 4,
 		"title" : "EarthJustice",
 		"description" : "EarthJustice is the largest nonprofit environmental law organization in the country, working to protect wildlife, for healthy communities, and for cleaner energy options. The organization represents its clients free of charge.",
 		"relatedPersonIds" : [2, 5, 8, 11, 14],
-		"totalDonations" : "$6.8m"		
+		"totalDonations" : "$6.8m",
+		"imageUrl" : "https://yt3.ggpht.com/-W_3EWqxKSLM/AAAAAAAAAAI/AAAAAAAAAAA/43OT-enqW4Y/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"
 	},
 	{
 		"organizationId" : 5,
 		"title" : "CrowdPac",
 		"description" : "We want to fight the power of Big Money in politics by helping small dollar donors engage in the political process. We want more citizens to vote and run for office. And we want to give people a platform to take action - whether it’s by joining a political community, or organizing one of their own.",
 		"relatedPersonIds" : [2, 5, 8, 11, 14],
-		"totalDonations" : "$1.5m"		
+		"totalDonations" : "$1.5m",
+		"imageUrl" : "https://res.cloudinary.com/crowdpac/image/upload/logo-square.jpg"	
 	},
 	{
 		"organizationId" : 6,
 		"title" : "Heritage Foundation",
 		"description" : "The mission of The Heritage Foundation is to formulate and promote conservative public policies based on the principles of free enterprise, limited government, individual freedom, traditional American values, and a strong national defense.",
 		"relatedPersonIds" : [1, 4, 7, 10, 13],
-		"totalDonations" : "$5.2m"
+		"totalDonations" : "$5.2m",
+		"imageUrl" : "http://dailysignal.com/wp-content/themes/daily-signal/assets/images/brand/the-heritage-foundation-logo-blue.png"
 	},
 	{
 		"organizationId" : 7,
 		"title" : "Planned Parenthood",
 		"description" : "Planned Parenthood is a trusted health care provider, an informed educator, a passionate advocate, and a global partner helping similar organizations around the world. Planned Parenthood delivers vital reproductive health care, sex education, and information to millions of people worldwide.",
 		"relatedPersonIds" : [1, 4, 7, 10, 13],
-		"totalDonations" : "$2.4m"
+		"totalDonations" : "$2.4m",
+		"imageUrl" : "https://upload.wikimedia.org/wikipedia/en/thumb/2/2c/Planned_Parenthood_logo.svg/1200px-Planned_Parenthood_logo.svg.png"
 	}
 ]);
 
