1c97a738fab4cb1621a08fa8481d75466cfe4c7d
Kevin Mark <kmark1625@gmail.com>
2017-10-21 21:46:16 -0700

    Add updates for button endpoint

--- server/routes/button.js
+++ server/routes/button.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.status(200).send(
         {
-            "Success": "Successfully retreived button url", 
+            "Success": "Successfully retrieved button url", 
             "url": "http://api.devexhacks.com/oauth2/authorize?client_id=vgw3sf4f8nq3b98i1gdfr8wpx4gpty0ska52&grant_type=authorization_code&redirect_uri=http://mywebsite.com&scope=verify&response_type=code"
         }
     );
